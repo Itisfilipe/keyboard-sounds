6422958f2c04046eb254c3bde3941523aa6f22dc
Filipe T. Amaral <filipetamaral@gmail.com>
2016-06-20 02:47:26 -0300

    simplified audioplayer, listener configured

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,34 @@
 'use strict';
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as child_process from 'child_process';
-let memoizee = require('memoizee');
-
+import {spawn} from 'child_process';
+// import * as events from 'events';
 
 export function activate(context: vscode.ExtensionContext) {
+
+    let basePath: string =  path.join(__dirname,'..', '..');
+    let spaceAudio: string = path.join(basePath, 'audio', 'spacebar_press.mp3');
+    let deleteAudio: string = path.join(basePath, 'audio', 'delete_press.mp3');
+    let otherKeysAudio: string = path.join(basePath, 'audio', 'key_press.mp3');
+
+    let player: AudioPlayer;
 
     console.log('Congratulations, your extension "keyboard-sounds" is now active!');
 
     let disposable = vscode.commands.registerCommand('xfilipe_keyboard_sounds.activate', () => {
-        // The code you place here will be executed every time your command is executed
-        let player = new AudioPlayer();
-        // player.playEnter();
-        player.playOtherKeys();
-        player.playOtherKeys();
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        player = player || new AudioPlayer();
+        let disposables: vscode.Disposable[] = [];
+
+        vscode.workspace.onDidChangeTextDocument((e)=> {
+            let pressedKey = e.contentChanges[0].text;
+            if (pressedKey == ""){
+                player.play(deleteAudio);
+            } else if (pressedKey == " "){
+                player.play(spaceAudio);
+            }  else {
+                player.play(otherKeysAudio);
+            }
+        }, this, disposables);
     });
 
     context.subscriptions.push(disposable);
@@ -27,48 +39,25 @@ export function deactivate() {
 }
 
 /**
- * Javascript keycodes, more here http://keycode.info/
- */
-enum keyCodes {BACKSPACE=8, ENTER=13, SPACE=32, DELETE=46}
-
-/**
  * Audio player for keystroke sounds
  */
 class AudioPlayer {
-    private _basePath: string =  path.join(__dirname,'..', '..');
-    private _spaceAudio: string = path.join(this._basePath, 'audio', 'spacebar_press.mp3');
-    private _deleteAudio: string = path.join(this._basePath, 'audio', 'delete_press.mp3');
-    private _otherKeysAudio: string = path.join(this._basePath, 'audio', 'key_press.mp3');
-    // uses memoizee to cache the exec function
-    // not sure if get fastter with this, TODO: test it
-    private _execCached = memoizee(child_process.exec, {primitive: true});
+    private _process: any = undefined;
+    private _stopped:boolean = true
 
-    _play(soundFile){
-        // TODO: remember to do that using binaries packed with this plugin for portability!
-        this._execCached(`mplayer -really-quiet -nolirc -ao alsa ${soundFile}`, error => {
-            if (error) {
-                vscode.window.showErrorMessage("You should have \"mplayer\" installed on your PATH,"+
-                                               " see the documentation for more information.")
-                return;
-            }
-        });
+    play(filePath: string){
+        if(!this._stopped)
+            this.stop()
+
+        this._stopped = false;
+        let args = ["-ao", "alsa", filePath];
+        this._process = spawn('mplayer', args);
     }
 
-    playEnter(): void{
-        this._play(this._deleteAudio)
-    }
-
-    playSpace(): void{
-        this._play(this._spaceAudio);
-    }
-
-    playOtherKeys(): void{
-        this._play(this._otherKeysAudio);
+    stop(){
+        if (this._process) {
+            this._process.kill('SIGTERM');
+        }
+        this._stopped = true;
     }
 }
-
-// class EditorListener {
-//     constructor(public player: AudioPlayer){
-
-//     }
-// }
