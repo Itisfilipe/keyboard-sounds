3b65ef174b2e43b598a8725b7c8425aad22ed1e0
Dean Schulz <dean.schulz@americaneagle.com>
2024-02-22 21:02:13 -0600

    Matrix sounds feature

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,15 @@ let player: AudioPlayer;
 let listener: EditorListener;
 let extensionPos: number;
 let isActive: boolean;
+let isMatrix: boolean;
 
+export function matrix_enable(context: vscode.ExtensionContext) {
+    isMatrix = context.globalState.get("xfilipe_keyboard_sounds.isMatrix", true);
+    this.activate();
+}
+export function matrix_disable(context: vscode.ExtensionContext) {
+    isMatrix = context.globalState.get("xfilipe_keyboard_sounds.isMatrix", false);
+}
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "keyboard-sounds" is now active!');
 
@@ -17,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
     listener = listener || new EditorListener(player);
 
     // is the extension activated? yes by default.
-    isActive = context.globalState.get("xfilipe_keyboard_sounds", true)
+    isActive = context.globalState.get("xfilipe_keyboard_sounds", true);
 
     vscode.commands.registerCommand('xfilipe_keyboard_sounds.activate', () => {
         if (!isActive) {
@@ -35,6 +43,25 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.showInformationMessage("Extension deactived.");
         } else {
             vscode.window.showWarningMessage("Extension already deactivated.");
+        }
+    });
+    vscode.commands.registerCommand('xfilipe_keyboard_sounds.enable_matrix', () => {
+        if (!isMatrix) {
+            context.globalState.update("xfilipe_keyboard_sounds.isMatrix", true);
+            isActive = true;
+            isMatrix = true;
+            vscode.window.showInformationMessage("Matrix sounds enabled.");
+        } else {
+            vscode.window.showWarningMessage("Extension has matrix sounds enabled already.");
+        }
+    });
+    vscode.commands.registerCommand('xfilipe_keyboard_sounds.disable_matrix', () => {
+        if (isMatrix) {
+            context.globalState.update("xfilipe_keyboard_sounds.isMatrix", false);
+            isMatrix = false;
+            vscode.window.showInformationMessage("Matrix sounds disabled.");
+        } else {
+            vscode.window.showWarningMessage("Extension has matrix sounds disabled already.");
         }
     });
 
@@ -57,6 +84,19 @@ export class EditorListener {
     private _deleteAudio: string = path.join(this._basePath, 'audio', 'delete_press.mp3');
     private _enterAudio: string = path.join(this._basePath, 'audio', 'enter_press.wav');
     private _otherKeysAudio: string = path.join(this._basePath, 'audio', 'key_press.mp3');
+    
+    //Matrix sounds
+    private _spaceAudioM: string = path.join(this._basePath, 'audio/matrix-key-sounds', 'matrix-key.wav');
+    private _deleteAudioM: string = path.join(this._basePath, 'audio/matrix-key-sounds', 'matrix-key-3.wav');
+    private _enterAudioM: string = path.join(this._basePath, 'audio/matrix-key-sounds', 'enter-audio.wav');
+    private _otherKeysAudioM1: string = path.join(this._basePath, 'audio/matrix-key-sounds', 'matrix-key.wav');
+    private _otherKeysAudioM2: string = path.join(this._basePath, 'audio/matrix-key-sounds', 'matrix-key-2.wav');
+    private _otherKeysAudioM3: string = path.join(this._basePath, 'audio/matrix-key-sounds', 'matrix-key-3.wav');
+    private _otherKeysArr: Array<string> = [
+        this._otherKeysAudioM1,
+        this._otherKeysAudioM2,
+        this._otherKeysAudioM3
+    ];
 
     constructor(private player: AudioPlayer) {
         vscode.workspace.onDidChangeTextDocument(this._keystrokeCallback, this, this._subscriptions);
@@ -70,17 +110,23 @@ export class EditorListener {
         let enteredText = e.contentChanges[0].text;
         if (enteredText == "") {
             // backspace or delete pressed
-            this.player.play(this._deleteAudio);
+            this.player.play(isMatrix ? this._deleteAudioM : this._deleteAudio);
         } else if (enteredText == " ") {
             // space pressed
-            this.player.play(this._spaceAudio);
+            this.player.play(isMatrix ? this._spaceAudioM : this._spaceAudio);
         } else if (enteredText == "\n" || enteredText.length > 2) {
             // enter pressed (length > 2 is a proxy for enter - it happens when autocomplete is used)
             // we use >2 instead of >1 so this this won't trigger when user enters { and it autocompletes to {}
-            this.player.play(this._enterAudio);
+            this.player.play(isMatrix ? this._enterAudioM : this._enterAudio);
         } else {
             // any other keys
-            this.player.play(this._otherKeysAudio);
+            if (isMatrix) {
+                const randomNumber = Math.floor(Math.random()*this._otherKeysArr.length);
+                this.player.play(this._otherKeysArr[randomNumber]);
+            }
+            else {
+                this.player.play(this._otherKeysAudio);
+            }
         }
     }
 
